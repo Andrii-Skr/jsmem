8e3aeebdd32f898a9091161ff2268ccc8ba392fa
KozaCarry <goodarky@gmail.com>
2021-07-12 17:57:22 +0300

    add url load
    добавил загрузку по url

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var canv=document.getElementById("img"),
 c=canv.getContext("2d");
 
+var imageUrl = document.getElementById('url1').value;
+
 function onfil(doc)
 {
     var file=doc.files[0],
@@ -16,15 +18,49 @@ function onfil(doc)
             c.drawImage(img,0,0);
             c.font="60px Arial";
             var text=document.getElementById('text1').value;
-            c.fillText(document.getElementById('text1').value ,canv.width/2-text.length/2*30,canv.height/2-15);
+            c.fillText(document.getElementById('text1').value ,canv.width/2-text.length/2*30,canv.height/4-15);
         }
     }
      fileread.readAsDataURL(file);
 };
+
+ butt.onclick = function()
+ {
+   
+     var img=new Image();
+     img.src= document.getElementById('url1').value;
+     img.onload=function()
+     {
+     canv.width=img.width;
+     canv.height=img.height;
+     c.drawImage(img,0,0);
+     c.font="60px Arial";
+     var text=document.getElementById('text1').value;
+     c.fillText(document.getElementById('text1').value ,canv.width/2-text.length/2*30,canv.height/4-15);
+     }
+   
+ };
+
+
 function get()
 {
     var link=document.createElement("a");
     link.download="download";
     link.href=canv.toDataURL(["image/png"]);
     link.click();
+    
+}
+
+function openMem(evt, Mem) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(Mem).style.display = "block";
+    evt.currentTarget.className += " active";
 }
